edd8163688b70b0992e3acff306d1fa476aa489b
aishanoor1439 <aisha.noor1439@gmail.com>
2024-11-20 10:33:28 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_no_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = false;
+
+            //Input: Number
+            Console.WriteLine("Enter the number you want to check:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //Initialization
+            int i = 2;
+
+            //Condition
+            while (i < Math.Sqrt(num))
+            {
+                //To check whether the number is prime or not
+                if (num % i == 0) 
+                {
+                    isPrime = true;
+                    
+                }
+                else
+                {
+                    isPrime = false;
+                    
+                }
+
+                //Incrementation
+                i++;
+            }
+
+            //Output
+            if (isPrime) 
+            {
+                Console.WriteLine("{0} is a prime number.", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a composite number.",num);
+            }
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Prime no Checker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prime no Checker")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ac8056af-81b8-48f9-ad7b-ab0404da5614")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
